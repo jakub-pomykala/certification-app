a4d29e8f19af7dba97efe7ade0c6fb0c4dbae3b1
Jakub Pomykala <jakub.pomykala@hotmail.co.uk>
2022-11-23 20:54:11 +0000

    Removing things that will not be given to the user in the exam

--- start/src/main/java/io/openliberty/exam/rest/ArtistApplication.java
+++
@@ -1,9 +0,0 @@
-package io.openliberty.exam.rest;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("artists")
-public class ArtistApplication extends Application {
-
-}
